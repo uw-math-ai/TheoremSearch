0a034470ef57792dde0ee06311b94ebba4b660f4
ericleonen <ericleonen@gmail.com>
2025-12-13 12:04:48 -0800

    Create latexdiff flattenting test

--- ec2/parse_arxiv_papers/regex_method/parse.py
+++ ec2/parse_arxiv_papers/regex_method/parse.py
@@ -1,4 +1,14 @@
 from typing import Set
+from ..main_tex import get_main_tex_path
+import os
+from .flatten import flatten_tex
 
 def parse_by_regex(paper_id: str, src_dir: str, theorem_types: Set[str]):
-    pass
+    main_tex_path = get_main_tex_path(src_dir)
+    main_tex_name = os.path.basename(main_tex_path)
+
+    paper_tex = flatten_tex(main_tex_name, src_dir)
+
+    print(paper_tex)
+
+    return []
