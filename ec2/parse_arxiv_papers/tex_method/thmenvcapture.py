eeb35bad2e27e5b1c0236764c5ca3e3969a6a236
ericleonen <ericleonen@gmail.com>
2025-12-14 23:32:43 -0800

    Add conservative freezing patterns

--- ec2/parse_arxiv_papers/tex_method/thmenvcapture.py
+++ ec2/parse_arxiv_papers/tex_method/thmenvcapture.py
@@ -58,22 +58,98 @@ def _insert_thmenvcapture_sty(
   \thmenvcapture@expandoktrue
   \begingroup
     \edef\thmenvcapture@bodystr{\detokenize{#1}}%
-    \in@{\\begin}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\end}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\item}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\par}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\verb}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\Verbatim}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\lstinline}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\mint}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\footnote}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\marginpar}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\cite}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\ref}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\eqref}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\input}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\include}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
-    \in@{\\includegraphics}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\edef\thmenvcapture@bodystr{\detokenize{#1}}%
+
+% --- structure / environments / paragraphing ---
+\in@{\\begin}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\end}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\item}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\par}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\noindent}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\indent}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\everypar}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\obeylines}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\obeyspaces}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+
+% --- verbatim / listings / minted / fragile catcode regimes ---
+\in@{\\verb}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\Verbatim}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\verbatim}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\lstinline}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\lstlisting}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\mint}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\minted}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+
+% --- footnotes / margin notes / floats (often write to aux) ---
+\in@{\\footnote}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\footnotemark}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\footnotetext}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\marginpar}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\caption}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+
+% --- cross-referencing / citations / indexing (moving-arg + aux writes) ---
+\in@{\\label}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\ref}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\eqref}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\pageref}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\autoref}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\cref}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\Cref}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\hyperref}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\href}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\cite}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\citep}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\citet}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\index}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\gls}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\Gls}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\glossary}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+
+% --- file IO / inclusion / external resources ---
+\in@{\\input}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\include}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\includeonly}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\usepackage}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\RequirePackage}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\openin}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\openout}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\read}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\write}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\write18}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\immediate}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+
+% --- graphics / TikZ / PGF (often huge expansions, file reads, catcode games) ---
+\in@{\\includegraphics}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\graphicspath}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\pgf}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\tikz}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\begin{tikzpicture}}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+
+% --- catcode / definition / expansion primitives (dangerous under xdef) ---
+\in@{\\catcode}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\code}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\makeatletter}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\makeatother}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\def}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\edef}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\xdef}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\gdef}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\let}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\futurelet}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\expandafter}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\noexpand}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\csname}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+
+% --- boxes / alignment / math displays (can be OK, but often explode) ---
+\in@{\\halign}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\valign}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\vbox}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\hbox}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\raise}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+
+% --- tables / arrays (often okay, but can expand a lot + alignment) ---
+\in@{\\tabular}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
+\in@{\\array}{\thmenvcapture@bodystr}\ifin@\global\thmenvcapture@expandokfalse\fi
   \endgroup
 }
 
