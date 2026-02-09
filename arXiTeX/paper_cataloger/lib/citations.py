e090f44c1c9531734d925a8aa781e17f751dd795
ericleonen <ericleonen@gmail.com>
2026-02-08 18:48:59 -0800

    Add sleeping schedule

--- arXiTeX/paper_cataloger/lib/citations.py
+++ arXiTeX/paper_cataloger/lib/citations.py
@@ -2,10 +2,12 @@
 Helpers to check if a paper high enough quality to attempt to parse.
 """
 
+import time
+import math
 import requests
 from typing import List
 
-def fetch_paper_citations(arxiv_ids: List[str]) -> List[int | None]:
+def fetch_paper_citations(arxiv_ids: List[str], retries: int = 3) -> List[int | None]:
     """
     Fetches the count of a paper's citations by its arXiv id. First, tries Semantic Scholar, then
     OpenAlex, then returns None.
@@ -14,6 +16,8 @@ def fetch_paper_citations(arxiv_ids: List[str]) -> List[int | None]:
     ----------
     arxiv_ids : List[str]
         List of papers' arXiv ids.
+    retries : int
+        Number of retries allowed for an empty citations batch.
 
     Returns
     -------
@@ -22,24 +26,35 @@ def fetch_paper_citations(arxiv_ids: List[str]) -> List[int | None]:
     
     """
 
-    arxiv_ids = ["ARXIV:" + arxiv_id.split("v")[0] for arxiv_id in arxiv_ids] # remove version just in case!
+    arxiv_ids_formatted = ["ARXIV:" + arxiv_id.split("v")[0] for arxiv_id in arxiv_ids] # remove version just in case!
 
-    try: # search Semantic Scholar 
+    try: # search Semantic Scholar
         scholar_res = requests.post(
             f"https://api.semanticscholar.org/graph/v1/paper/batch",
             params={"fields": "citationCount"},
-            json={"ids": arxiv_ids}
+            json={"ids": arxiv_ids_formatted}
         )
 
         if scholar_res.ok:
             scholar_data = scholar_res.json()
             
             ks = [paper_json.get("citationCount") if paper_json else None for paper_json in scholar_data]
+
+            if all(k is None for k in ks):
+                raise ValueError("Empty citations")
             
             return ks
             
-    except Exception as e:
-        pass
+    except Exception:
+        if retries > 0:
+            sleep_s = len(arxiv_ids)**(1/retries)
+            time.sleep(sleep_s)
+
+            print(f"Sleeping {sleep_s}s for SemanticScholar")
+
+            return fetch_paper_citations(arxiv_ids, retries - 1)
+        else:
+            pass
 
     # try: # search OpenAlex by arXiv id
     #     alex_res = requests.get(f"https://api.openalex.org/works/doi:10.48550/arXiv.{arxiv_id}", timeout=10)
