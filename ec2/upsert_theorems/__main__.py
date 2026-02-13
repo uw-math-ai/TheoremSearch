de1871cd5271ee66510a2861de3bc160dfc70a4a
ericleonen <ericleonen@gmail.com>
2026-02-12 21:21:08 -0800

    Delete old parsing code

--- ec2/upsert_theorems/__main__.py
+++ ec2/upsert_theorems/__main__.py
@@ -15,63 +15,91 @@ from tqdm import tqdm
 THEOREM_TYPES = { "theorem", "lemma", "proposition", "corollary" }
 
 def _upsert_theorems_from_json(
-    drive_link: str,
+    folder_link: str,
     source: str,
-    title: Optional[str] = None,
-    source_link: Optional[str] = None,
-    paper_id: Optional[str] = None
+    source_link: str
 ):
-    if not title:
-        title = source
-    if not paper_id:
-        paper_id = source.replace(" ", "-").lower()
+    source_id = source.lower().replace(" ", "-")
 
     # TODO: Use TemporaryDirectory instead
-    source_file = Path(f"DEBUG/{paper_id}.json")
+    source_folder = Path(f"DEBUG/{source_id}")
 
-    gdown.download(drive_link, str(source_file))
+    # gdown.download_folder(url=folder_link, output=str(source_folder), remaining_ok=True)
 
-    source_json = json.load(source_file.open("r", errors="ignore"))
-    source_link = source_link or source_json["source"]
+    # return
 
     conn = get_rds_connection()
 
-    with conn.cursor() as cur:
-        upsert_row(
-            cur,
-            table="paper",
-            row={
-                "paper_id": paper_id,
-                "source": source,
-                "title": title,
-                "authors": [], # TODO: Add support
-                "link": source_link
-            }
+    for source_file in source_folder.iterdir():
+        source_json = json.load(source_file.open("r", errors="ignore"))
+
+        title = source_json["original_path"] \
+            .replace(".tex", "") \
+            .replace("ch", "Chapter ") \
+            .replace("app", "Appendix ")
+        
+        title_segments = title.split("-")
+        name_start = next(
+            i + 1 for i in range(len(title_segments)-1, -1, -1)
+            if title_segments[i] and title_segments[i][0].isdigit()
         )
+        title = ".".join(title_segments[:name_start]) + " " + " ".join(s.capitalize() for s in title_segments[name_start:])
+            
+        paper_id = source_id + "_" + title.replace(": ", "_").replace(" ", "-").lower()
 
-    conn.commit()
+        with conn.cursor() as cur:
+            upsert_row(
+                cur,
+                table="paper",
+                row={
+                    "paper_id": paper_id,
+                    "source": source,
+                    "title": title,
+                    "authors": ["Jarod Alper"], # TODO: Add support
+                    "link": ""
+                },
+                on_conflict={
+                    "with": ["paper_id"],
+                    "replace": ["title"]
+                }
+            )
 
-    batch_theorems = []
+        conn.commit()
 
-    for theorem in tqdm(source_json["theorems"], dynamic_ncols=True):
-        if not theorem["type"] in THEOREM_TYPES:
-            continue
+        batch_theorems = []
 
-        if "theorem_name" not in theorem:
-            raise ValueError("theorem_name not in theorem")
-        elif "body" not in theorem:
-            raise ValueError("body not in theorem")
+        print(title)
+        for theorem in tqdm(source_json["theorems"], dynamic_ncols=True):
+            if not theorem["type"] in THEOREM_TYPES:
+                continue
 
-        batch_theorems.append({
-            "paper_id": paper_id,
-            "name": theorem["theorem_name"],
-            "body": theorem["body"],
-            "label": theorem.get("label", None),
-            "link": theorem.get("link", None) or theorem.get("url", None),
-            "parsing_method": "regex"
-        })
+            if "theorem_name" not in theorem:
+                raise ValueError("theorem_name not in theorem")
+            elif "body" not in theorem:
+                raise ValueError("body not in theorem")
 
-        if len(batch_theorems) == 128:
+            batch_theorems.append({
+                "paper_id": paper_id,
+                "name": theorem["theorem_name"],
+                "body": theorem["body"],
+                "label": theorem.get("label", None),
+                "link": theorem.get("link", None) or theorem.get("url", None),
+                "parsing_method": "regex"
+            })
+
+            if len(batch_theorems) == 128:
+                with conn.cursor() as cur:
+                    upsert_rows(
+                        cur,
+                        table="theorem",
+                        rows=batch_theorems
+                    )
+
+                conn.commit()
+
+                batch_theorems = []
+
+        if len(batch_theorems) > 0:
             with conn.cursor() as cur:
                 upsert_rows(
                     cur,
@@ -81,27 +109,15 @@ def _upsert_theorems_from_json(
 
             conn.commit()
 
-            batch_theorems = []
-
-    if len(batch_theorems) > 0:
-        with conn.cursor() as cur:
-            upsert_rows(
-                cur,
-                table="theorem",
-                rows=batch_theorems
-            )
-
-        conn.commit()
-
     conn.close()
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser()
 
     arg_parser.add_argument(
-        "--link",
-        required=True,
-        help="Google Drive link to JSON file"
+        "--folder-link",
+        required=False,
+        help="Google Drive link to folder of JSON files"
     )
 
     arg_parser.add_argument(
@@ -110,9 +126,16 @@ if __name__ == "__main__":
         help="Name of source"
     )
 
+    arg_parser.add_argument(
+        "--source-link",
+        required=True,
+        help="Link to source"
+    )
+
     args = arg_parser.parse_args()
 
     _upsert_theorems_from_json(
-        drive_link=args.link,
-        source=args.source
+        folder_link=args.folder_link,
+        source=args.source,
+        source_link=args.source_link
     )
