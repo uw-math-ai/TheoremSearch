537b5cda409bce44043ac2586b6a78f21c3115e3
ericleonen <ericleonen@gmail.com>
2026-02-25 13:02:41 -0800

    Create basic \cite[...]{...} interpaper dependency connector script

--- rds/core/theorem_dependency.sql
+++ rds/core/theorem_dependency.sql
@@ -2,17 +2,19 @@ CREATE TABLE theorem_dependency (
     src_theorem_id UUID NOT NULL REFERENCES theorem(id) ON DELETE CASCADE,
     dep_key TEXT NOT NULL, -- either a theorem label or a paper BibTeX key
     interpaper BOOLEAN NOT NULL,
+    dep_theorem_name TEXT NOT NULL DEFAULT '', -- e.g. "Theorem A.1", empty string for paper-level deps
     dep_paper_id TEXT NULL,
     dep_paper_source TEXT NULL,
     dep_theorem_id UUID NULL REFERENCES theorem(id) ON DELETE CASCADE,
-    
+
     FOREIGN KEY (dep_paper_id, dep_paper_source) REFERENCES paper (id, source)
-    ON DELETE CASCADE,
+        ON DELETE CASCADE,
 
     CONSTRAINT dep_paper_xor_theorem CHECK (
         -- Inter-paper, resolved to paper only
         (
             interpaper IS TRUE
+            AND dep_theorem_name = ''
             AND dep_paper_id IS NOT NULL
             AND dep_paper_source IS NOT NULL
             AND dep_theorem_id IS NULL
@@ -21,21 +23,32 @@ CREATE TABLE theorem_dependency (
         -- Inter-paper, resolved to specific theorem
         (
             interpaper IS TRUE
+            AND dep_theorem_name <> ''
             AND dep_paper_id IS NULL
             AND dep_paper_source IS NULL
             AND dep_theorem_id IS NOT NULL
         )
         OR
+        -- Inter-paper, resolved to specific theorem ref but paper not in DB
+        (
+            interpaper IS TRUE
+            AND dep_theorem_name <> ''
+            AND dep_paper_id IS NOT NULL
+            AND dep_paper_source IS NOT NULL
+            AND dep_theorem_id IS NULL
+        )
+        OR
         -- Intra-paper, always theorem-level
         (
             interpaper IS FALSE
+            AND dep_theorem_name = ''
             AND dep_paper_id IS NULL
             AND dep_paper_source IS NULL
             AND dep_theorem_id IS NOT NULL
         )
     ),
 
-    PRIMARY KEY (src_theorem_id, dep_key)
+    PRIMARY KEY (src_theorem_id, dep_key, dep_theorem_name)
 );
 
 COMMENT ON TABLE theorem_dependency IS
